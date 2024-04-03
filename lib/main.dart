daea226c88dc43e1c7b2ffa5c2a73aa6ab42ccd1
Mark Choi <mark.tangible@gmail.com>
2024-04-03 18:05:09 +0900

    intl, flutter_localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_playground/screens/home_screen.dart';
 
 void main() {
   runApp(
-    const ProviderScope(child: MaterialApp(home: HomeScreen()))
+    const ProviderScope(child:
+      MaterialApp(
+        title: "Riverpod starter",
+        localizationsDelegates: [GlobalMaterialLocalizations.delegate],
+        supportedLocales: [
+          Locale('en'),
+          Locale('ko')
+        ],
+        home: HomeScreen())
+      )
   );
 }
